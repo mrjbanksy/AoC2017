1cc3951fb3440e2e8f6c1631b147abff3a17afeb
Jeremy Banks <mrjbanksy@gmail.com>
2017-12-18 11:40:26 -0500

    day 4

---
+++ src/main/java/day4/Day4Part1.java
@@ -0,0 +1,49 @@
+package day4;
+
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+import day2.Day2Part1;
+
+public class Day4Part1 {
+	private static final String inputFile = "/day4/input.txt";
+
+	public static void main(String[] args) {
+		InputStream inputFileStream = Day2Part1.class.getResourceAsStream(inputFile);
+		if (inputFileStream == null) {
+			System.out.println("Unable to open file at " + inputFile);
+			System.exit(1);
+		}
+		int validPhrases = getValidPhrases(inputFileStream);
+		System.out.println("The number of valid passphrases is: " + validPhrases);
+	}
+
+	private static int getValidPhrases(InputStream inputFileStream) {
+		Scanner inputScanner = new Scanner(inputFileStream);
+		int validCount = 0;
+		while (inputScanner.hasNextLine()) {
+			String phrase = inputScanner.nextLine();
+			validCount += checkPhrase(phrase);
+		}
+		inputScanner.close();
+		return validCount;
+	}
+
+	/**
+	 * @param phrase The phrase to check
+	 * @return 1 if valid, 0 if not so I don't have to do a conditional check with a boolean above
+	 */
+	private static int checkPhrase(String phrase) {
+		String[] words = phrase.split("\\s+");
+		Set<String> wordset = new HashSet<>();
+		for (String word : words) {
+			if (!wordset.add(word)) {
+				return 0;
+			}
+		}
+		return 1;
+	}
+
+}
---
+++ src/main/java/day4/Day4Part2.java
@@ -0,0 +1,60 @@
+package day4;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+import day2.Day2Part1;
+
+public class Day4Part2 {
+	private static final String inputFile = "/day4/input.txt";
+
+	public static void main(String[] args) {
+		InputStream inputFileStream = Day2Part1.class.getResourceAsStream(inputFile);
+		if (inputFileStream == null) {
+			System.out.println("Unable to open file at " + inputFile);
+			System.exit(1);
+		}
+		int validPhrases = getValidPhrases(inputFileStream);
+		System.out.println("The number of valid passphrases is: " + validPhrases);
+	}
+
+	private static int getValidPhrases(InputStream inputFileStream) {
+		Scanner inputScanner = new Scanner(inputFileStream);
+		int validCount = 0;
+		while (inputScanner.hasNextLine()) {
+			String phrase = inputScanner.nextLine();
+			validCount += checkPhrase(phrase);
+		}
+		inputScanner.close();
+		return validCount;
+	}
+
+	/**
+	 * @param phrase The phrase to check
+	 * @return 1 if valid, 0 if not so I don't have to do a conditional check with a boolean above
+	 */
+	private static int checkPhrase(String phrase) {
+		String[] words = phrase.split("\\s+");
+		Set<Map<String, Integer>> wordSet = new HashSet<>();
+		for (String word : words) {
+			Map<String, Integer> characterMap = new HashMap<>();
+			String[] characters = word.split("");
+			for (String character : characters) {
+				if (characterMap.containsKey(character)) {
+					characterMap.put(character, characterMap.get(character) + 1);
+				} else {
+					characterMap.put(character, 1);
+				}
+			}
+			if (!wordSet.add(characterMap)) {
+				return 0;
+			}
+		}
+		return 1;
+	}
+
+}
