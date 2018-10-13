c2774b025a969a7b586ce70697fefcb145086fad
Jeremy Banks <mrjbanksy@gmail.com>
2018-10-13 17:55:52 -0400

    wip day 7

--- src/main/java/day07/Day7.java
+++ src/main/java/day07/Day7.java
@@ -6,12 +6,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
-public class Day7Part1 {
+public class Day7 {
 	private static final String inputFile = "/day07/input.txt";
 
 	public static void main(String[] args) {
-		InputStream inputFileStream = Day7Part1.class.getResourceAsStream(inputFile);
+		InputStream inputFileStream = Day7.class.getResourceAsStream(inputFile);
 		if (inputFileStream == null) {
 			System.out.println("Unable to open file at " + inputFile);
 			System.exit(1);
@@ -19,6 +20,9 @@ public class Day7Part1 {
 		Map<String, Program> programMap = parsePrograms(inputFileStream);
 		Program bottomProgram = getBottomProgram(programMap);
 		System.out.println("The bottom program is " + bottomProgram.getName());
+		Program unbalancedProgram = getUnbalancedProgram(bottomProgram);
+		int correctWeight = getCorrectWeight(unbalancedProgram);
+		System.out.println("The correct weight for the unbalanced program is: " + correctWeight);
 	}
 
 	private static Map<String, Program> parsePrograms(InputStream inputFileStream) {
@@ -60,5 +64,38 @@ public class Day7Part1 {
 			bottomProgram = bottomProgram.getParent();
 		}
 		return bottomProgram;
+	}
+
+	private static Program getUnbalancedProgram(Program program) {
+		List<Integer> childWeight = new ArrayList<Integer>();
+		for (Program child : program.getChildren()) {
+			childWeight.add(getTowerWeight(child));
+		}
+		List<Integer> sortedWeight = childWeight.stream().sorted().collect(Collectors.toList());
+		if (sortedWeight.get(0) == sortedWeight.get(sortedWeight.size() - 1)) {
+			return program;
+		} else {
+			for (Program child : program.getChildren()) {
+				return getUnbalancedProgram(child);
+			}
+		}
+		return null;
+	}
+
+	private static int getTowerWeight(Program program) {
+		int childWeight = 0;
+		for (Program child : program.getChildren()) {
+			childWeight += getTowerWeight(child);
+		}
+		return program.getWeight() + childWeight;
+	}
+	
+	private static int getCorrectWeight(Program unbalancedProgram) {
+		Program parent = unbalancedProgram.getParent();
+		for (Program child : parent.getChildren()) {
+			System.out.println("This child's weight is " + child.getWeight());
+			System.out.println("Its tower weight is " + getTowerWeight(child));
+		}
+		return 0;
 	}
 }
